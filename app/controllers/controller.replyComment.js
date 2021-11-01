1c948807e9c645b6ad994fd4207e96aaaf0ec515
unknown <chautien.it@gmail.com>
2021-11-01 08:14:51 +0700

    feat: log app port when run app

--- app/controllers/controller.replyComment.js
+++ app/controllers/controller.replyComment.js
@@ -1,7 +1,9 @@
 const ReplyCommentModel = require('../models/model.replyComment');
 
 class ReplyCommentController {
-  add = async (req, res) => {};
+  add = async (req, res) => {
+    const {} = req.params
+  };
 }
 
 module.exports = new ReplyCommentController();
