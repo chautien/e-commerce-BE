4824679bb80b829436001c9a6259fe1a5e16236c
Tiến Châu <chautien.it@gmail.com>
2021-11-15 09:00:47 +0700

    fix: update dir for static file

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ require('dotenv').config();
 
 var app = express();
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views/'));
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'views/template')));
+app.use(express.static(path.join(__dirname, 'views/template/')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/')));
 
 database.connect;
 App.init(app);
--- routes/router.admin.js
+++ routes/router.admin.js
@@ -3,10 +3,10 @@ const adminController = require('../app/controllers/controller.admin');
 const adminAuth = require('../middlewares/adminAuth');
 
 Router.get('/', adminAuth, (req, res) => {
-  res.status(200).render('index.ejs');
+  res.status(200).render('template/index.ejs');
 });
 Router.get('/login', (req, res) => {
-  res.status(200).render('auth/login.ejs');
+  res.status(200).render('template/auth/login.ejs');
 });
 Router.get('/logout', adminController.logout);
 Router.post('/login', adminController.login);
