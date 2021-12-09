45386c250cf4cb7d59a41ae67b85696abaaad22c
Tiến Châu <chautien.it@gmail.com>
2021-12-09 10:14:24 +0700

    fix: cors

--- app.js
+++ app.js
@@ -19,8 +19,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public/')));
 app.use(express.static(path.join(__dirname, 'storage/')));
 app.use(nocache());
-app.use((req, res, next) => {
+app.all('/*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   next();
 });
 
